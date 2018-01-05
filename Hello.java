cef810bc58ba4b1f597c112852ef727bf5d22fdf
zzb123456789 <1270023916@qq.com>
2018-01-05 23:11:42 +0800

    第五次

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 趙志博  000003");
-		System.out.println("Hello World 趙志博  000003");
-		System.out.println("Hello World 趙志博  000003");
+		System.out.println("Hello World 趙志博 999999");
         	}
 
 }
